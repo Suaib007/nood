645ffb94a9db213ce391921d63456c5c6f33aa76
Suaib007 <suaibsaifi07@gmail.com>
2024-12-03 21:11:49 +0530

    allow host

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ynb^3b0llfpc08z8n%w+bqykcm&_b3r91=3q($@b%z#k+&e19b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
